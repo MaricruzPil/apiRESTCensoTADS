0c0757b9ae3412dd5a00233792afae017a965e33
maricruzpiw <l22200789@pachuca.tecnm.mx>
2026-02-18 19:25:04 -0600

    Versión original

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apiRESTCensoTADS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="apiRESTCensoTADS.WebApiApplication" Language="C#" %>
